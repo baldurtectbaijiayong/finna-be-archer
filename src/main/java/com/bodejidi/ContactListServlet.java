e2f1e5fd73e1efb17200d4f297a7cfa79cac715a
yuanchangjun <qinxiangdexunyicao@gmail.com>
2014-07-22 18:14:23 +0800

    连接数据库查找所有联系人姓名并输出

--- src/main/java/com/bodejidi/ContactListServlet.java
+++ src/main/java/com/bodejidi/ContactListServlet.java
@@ -6,10 +6,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class ContactListServlet extends HttpServlet{
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException{
             response.getWriter().println("All Contacts");
+            String sql = "select * from contact";
+            try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+            }catch(Exception e){
+                //ignore
+            }
+            try{
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+                while (resultSet.next()){
+                    response.getWriter().println("Name:" + resultSet.getString("name"));      
+                }
+                
+
+            }catch(SQLException sqle){
+                response.getWriter().println("Can not connect DB");
+            }
     }
 
 }
