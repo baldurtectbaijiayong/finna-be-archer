9088a55227a41282815235e337a39452458afff6
Dev--xu <xushuangshuang1@gmail.com>
2014-07-22 17:54:32 +0800

    完成驱动和URl的加载，连上了数据库

--- src/main/java/com/bodejidi/ShowContactServlet.java
+++ src/main/java/com/bodejidi/ShowContactServlet.java
@@ -6,11 +6,40 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 public class ShowContactServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) 
         throws ServletException, IOException
     {
-        resp.getWriter().println("Contact :");
+
+        String SQLDriver = "com.mysql.jdbc.Driver";
+        String SQLURL = "jdbc:mysql://localhost/test?" 
+        + "user=root&password=";
+
+        Connection connection = null;
+        //Long id = Long.valueOf(req.getParameter("ContactId"));
+
+        try
+        {
+            Class.forName(SQLDriver).newInstance();    
+        }catch(Exception ex)
+        {
+            //ignore;
+        }
+
+        try
+        {
+            connection = DriverManager.getConnection(SQLURL);
+        }catch(SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLStates: " + ex.getSQLState());
+            System.out.println("ErrorCode: " + ex.getErrorCode());
+        }
+
+        resp.getWriter().println(connection);
     } 
 }
