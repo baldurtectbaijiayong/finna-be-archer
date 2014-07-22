3011dc641a23e4999d3b56acc6d9b1409383a427
yuanchangjun <qinxiangdexunyicao@gmail.com>
2014-07-22 19:10:03 +0800

    把查到的数据放到Map中再放到List中

--- src/main/java/com/bodejidi/ContactListServlet.java
+++ src/main/java/com/bodejidi/ContactListServlet.java
@@ -12,29 +12,79 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
 public class ContactListServlet extends HttpServlet{
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException{
+
             response.getWriter().println("All Contacts");
-            String sql = "select * from contact";
+            String sql = "select contact.name,contact.mobile,department.name from contact,contact_department,department where contact.id = contact_department.id_contact and contact_department.id_department = department.id";
+            
+            List contacts = new ArrayList();            
+
+            Connection connection = null;
+            Statement statement = null;
+            ResultSet resultSet = null;
             try{
                 Class.forName("com.mysql.jdbc.Driver").newInstance();
             }catch(Exception e){
                 //ignore
             }
             try{
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(sql);
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+                statement = connection.createStatement();
+               
+                resultSet = statement.executeQuery(sql);
+                
                 while (resultSet.next()){
-                    response.getWriter().println("Name:" + resultSet.getString("name"));      
+                    Map contact = new HashMap();
+
+                    contact.put("name",resultSet.getString("contact.name"));
+                    contact.put("mobile",resultSet.getString("contact.mobile"));
+                    contact.put("department",resultSet.getString("department.name"));
+
+                    contacts.add(contact);
                 }
                 
 
             }catch(SQLException sqle){
                 response.getWriter().println("Can not connect DB");
             }
+
+            if(resultSet!=null){
+                try{
+                    resultSet.close();
+                }catch(Exception e){
+                    
+                }
+            }
+               
+             if(statement!=null){
+                try{
+                    statement.close();
+                }catch(Exception e){
+                        
+                }
+            }
+            if(connection!=null){
+                try{
+                    connection.close();
+                }catch(Exception e){
+
+                }
+            }
+            for(Object obj: contacts){
+
+                Map contact = (Map) obj;
+                response.getWriter().println("Name:" + contact.get("name"));
+                response.getWriter().println("mobile:" + contact.get("mobile"));
+                response.getWriter().println("department:" + contact.get("department"));
+            }   
     }
 
 }
