42dcde2f07fd5440d3c6ebee91b7cdb494a9f2f3
baijiayong <jiayongbai@gmail.com>
2014-07-26 15:59:49 +0800

    当地址栏无参数时转到显示所有联系人页面

--- src/main/java/com/bodejidi/AdministratorContactServlet.java
+++ src/main/java/com/bodejidi/AdministratorContactServlet.java
@@ -12,12 +12,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class AdministratorContactServlet extends HttpServlet{
     public void doGet(HttpServletRequest request,HttpServletResponse response)
         throws IOException,ServletException{
         
-        Contact contact = new Contact();
-        
+        String sql;
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
@@ -27,57 +29,134 @@ public class AdministratorContactServlet extends HttpServlet{
         }catch(Exception ex){
             //handl the error
         }
-        
-        try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select * from contact where id=" + request.getParameter("contactId"));      
-            resultSet.next();
-            contact.setId(resultSet.getLong("id"));
-            contact.setName(resultSet.getString("name"));
-            contact.setMobile(resultSet.getString("mobile"));
-            contact.setVpmn(resultSet.getString("vpmn"));
-            contact.setEmail(resultSet.getString("email"));
-            contact.setHomeAddress(resultSet.getString("home_address"));
-            contact.setOfficeAddress(resultSet.getString("office_address"));
-            contact.setMemo(resultSet.getString("memo"));
-            contact.setJob(resultSet.getString("job"));
-            contact.setJobLevel(resultSet.getLong("job_level"));
-            
-            System.out.println(resultSet);
-            request.setAttribute("contact",contact);
+        if(request.getParameter("contactId") != null && request.getParameter("contactId") != ""){
+            try{
+                sql = "select * from  department,contact,contact_department where "
+                    + "contact.id='" 
+                    + request.getParameter("contactId") 
+                    + "'and contact_department.id_department=department.id and" 
+                    + " contact.id=contact_department.id_contact";
+                    
+                Contact contact = new Contact();
                 
-            getServletContext()
-                .getRequestDispatcher("/WEB-INF/jsp/administrator/contact/show.jsp")
-                .forward(request, response);
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery(sql);      
+                resultSet.next();
                 
-        }catch(SQLException sqle){
-            response.getWriter().println("can not connect to DB");
-            response.getWriter().println(sqle.getMessage());
-            sqle.printStackTrace();
-        }
-        
-        if(resultSet != null){
-            try{
-                resultSet.close();
-            }catch(Exception ex){
-            
+                contact.setId(resultSet.getLong("contact.id"));
+                contact.setName(resultSet.getString("contact.name"));
+                contact.setMobile(resultSet.getString("mobile"));
+                contact.setVpmn(resultSet.getString("vpmn"));
+                contact.setEmail(resultSet.getString("email"));
+                contact.setHomeAddress(resultSet.getString("home_address"));
+                contact.setOfficeAddress(resultSet.getString("office_address"));
+                contact.setMemo(resultSet.getString("contact.memo"));
+                contact.setJob(resultSet.getString("job"));
+                contact.setJobLevel(resultSet.getLong("job_level"));
+                contact.setDepartment(resultSet.getString("department.name"));
+                contact.setDepartmentId(resultSet.getLong("department.id"));
+                
+                System.out.println(resultSet);
+                
+                request.setAttribute("contact",contact);
+                getServletContext()
+                    .getRequestDispatcher("/WEB-INF/jsp/administrator/contact/show.jsp")
+                    .forward(request, response);
+                    
+            }catch(SQLException sqle){
+                response.getWriter().println("can not connect to DB");
+                response.getWriter().println(sqle.getMessage());
+                sqle.printStackTrace();
             }
-        }
-        
-        if(statement != null){
-            try{
-                statement.close();
-            }catch(Exception ex){
             
+            if(resultSet != null){
+                try{
+                    resultSet.close();
+                }catch(Exception ex){
+                
+                }
             }
-        }
-        
-        if(connection != null){
-            try{
-                connection.close();
-            }catch(Exception e){
             
+            if(statement != null){
+                try{
+                    statement.close();
+                }catch(Exception ex){
+                
+                }
+            }
+            
+            if(connection != null){
+                try{
+                    connection.close();
+                }catch(Exception e){
+                
+                }
+            }
+        }else{
+            try{
+                sql = "select * from  department,contact,contact_department"
+                    + " where contact_department.id_department=department.id and" 
+                    + " contact.id=contact_department.id_contact";
+                    
+                List<Contact> contacts = new ArrayList();
+            
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery(sql);  
+                
+                while(resultSet.next()) {
+                    Contact contact = new Contact();
+                    
+                    contact.setId(resultSet.getLong("contact.id"));
+                    contact.setName(resultSet.getString("contact.name"));
+                    contact.setMobile(resultSet.getString("mobile"));
+                    contact.setVpmn(resultSet.getString("vpmn"));
+                    contact.setEmail(resultSet.getString("email"));
+                    contact.setHomeAddress(resultSet.getString("home_address"));
+                    contact.setOfficeAddress(resultSet.getString("office_address"));
+                    contact.setMemo(resultSet.getString("contact.memo"));
+                    contact.setJob(resultSet.getString("job"));
+                    contact.setJobLevel(resultSet.getLong("job_level"));
+                    contact.setDepartment(resultSet.getString("department.name"));
+                    contact.setDepartmentId(resultSet.getLong("department.id"));
+                    
+                    contacts.add(contact);
+                }
+                System.out.println("resultset: " + resultSet);
+               
+                request.setAttribute("contactList",contacts);
+                getServletContext()
+                    .getRequestDispatcher("/WEB-INF/jsp/administrator/contact/list.jsp")
+                    .forward(request, response);   
+            }catch(SQLException sqle){
+                response.getWriter().println("can not connect to DB");
+                response.getWriter().println(sqle.getMessage());
+                sqle.printStackTrace();
+            }
+            
+            if(resultSet != null){
+                try{
+                    resultSet.close();
+                }catch(Exception ex){
+                
+                }
+            }
+            
+            if(statement != null){
+                try{
+                    statement.close();
+                }catch(Exception ex){
+                
+                }
+            }
+            
+            if(connection != null){
+                try{
+                    connection.close();
+                }catch(Exception e){
+                
+                }
             }
         }
     }
---
+++ src/main/webapp/WEB-INF/jsp/administrator/contact/list.jsp
@@ -0,0 +1,33 @@
+<%@ page import="java.util.List,com.bodejidi.Contact,com.bodejidi.Department"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core"%>
+<% 
+    List contactList = (List) request.getAttribute("contactList");
+%>
+
+<html>
+    <head>
+        <title>
+            All Contacts
+        </title>
+    </head>
+    <body>
+        <table border = "1">
+            <h1>All Contacts</h1>
+            <tr>
+                <td>name</td>
+                <td>mobile</td>
+                <td>department</td>
+            
+            </tr>
+
+            <c:forEach var = "contact" items= "${contactList}">
+            <tr>
+                <td>${contact.name}</td>
+                <td>${contact.mobile}</td>
+                <td>${contact.department}</td>
+            </tr>
+            </c:forEach>
+        </table>
+    </body>
+
+</html>
