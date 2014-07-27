36507e86e3175b4f9ba3c2e9d80efa5d347f9e07
baijiayong <jiayongbai@gmail.com>
2014-07-27 17:17:34 +0800

    实现了添加部门的功能

--- src/main/java/com/bodejidi/AdministratorDepartmentCreateServlet.java
+++ src/main/java/com/bodejidi/AdministratorDepartmentCreateServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
@@ -21,6 +22,19 @@ public class AdministratorDepartmentCreateServlet extends HttpServlet {
      public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
         Connection connection = null;
+        Statement statement = null;
+        String sql;
+        Department department = new Department();
+        department.setName(request.getParameter("departmentName"));
+        department.setMemo(request.getParameter("departmentMemo"));
+        department.setParent(request.getParameter("departmentParent"));
+        department.setAddress(request.getParameter("departmentAddress"));
+        
+        sql = "insert into department (name, memo, parent, address) values('"
+            + department.getName() + "','"
+            + department.getMemo() + "','"
+            + department.getParent() + "','"
+            + department.getAddress() + "')";
         
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -30,10 +44,27 @@ public class AdministratorDepartmentCreateServlet extends HttpServlet {
         
         try{
             connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
-            response.getWriter().println(connection);
-            connection.close();
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            response.getWriter().println("success");
         } catch(SQLException sqle){
             response.getWriter().println(sqle);
         }
+        
+        if(statement != null){
+            try{
+                statement.close();
+            } catch(Exception e){
+                response.getWriter().println(e);
+            }
+        } 
+        
+        if(connection != null){
+            try{
+                connection.close();
+            } catch(Exception e){
+                response.getWriter().println(e);
+            }
+        } 
     }
 }
