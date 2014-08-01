8611d7a81f42e11f5f6a4712d9313dc7ddcd3ecf
hyfaqy <hyfaqy@gmail.com>
2014-08-01 10:10:54 +0800

    将DepartmentListServlet里面的关于数据库加载驱动，连接，执行，关闭数据库的操作利用DatabaseManager类替换。

--- src/main/java/com/bodejidi/DepartmentListServlet.java
+++ src/main/java/com/bodejidi/DepartmentListServlet.java
@@ -6,10 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.ResultSet;
 
 import java.util.Map;
@@ -20,24 +17,14 @@ import java.util.ArrayList;
 public class DepartmentListServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-        response.getWriter().println("Show");
-
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        
+        String sql = "select * from department";
+        DatabaseManager db = new DatabaseManager();
         List<Department> departments = new ArrayList();
-
+        db.connectAndCreateStatement();
+        ResultSet resultSet = db.executeQuery(sql);
+        
         try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-        } catch(Exception e) {
-            // handle the error
-        }
-
-        try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select * from department");
-
             while(resultSet.next()){
                 Department department = new Department();
                 
@@ -54,31 +41,8 @@ public class DepartmentListServlet extends HttpServlet {
             response.getWriter().println("cannot connect to db");
             sqle.printStackTrace();
         }
-
-        if(resultSet != null) {
-            try{
-                resultSet.close();
-            }catch(Exception ex) {
-                
-            }
-        }
-
-        if(statement != null) {
-            try{
-                statement.close();
-            }catch(Exception ex){
-
-            }
-        }
-
-        if(connection != null) {
-            try{
-                connection.close();
-            }catch(Exception ex){
-
-            }
-        }
-
+        db.close();
+        
         request.setAttribute("departmentList", departments);
         
         getServletContext()
