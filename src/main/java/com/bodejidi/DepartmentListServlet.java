4c0d1e9baa7d9de9753b032390d40561a6d955db
hyfaqy <hyfaqy@gmail.com>
2014-08-01 14:38:17 +0800

    进一步删除多余代码

--- src/main/java/com/bodejidi/DepartmentListServlet.java
+++ src/main/java/com/bodejidi/DepartmentListServlet.java
@@ -2,17 +2,8 @@ package com.bodejidi;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.sql.SQLException;
-import java.sql.ResultSet;
-
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 
 public class DepartmentListServlet extends AbstractFinnalServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
